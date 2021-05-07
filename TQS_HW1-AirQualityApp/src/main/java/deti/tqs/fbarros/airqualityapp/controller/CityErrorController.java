ecc495ecbdf1640f584d56a4261ebcb0fd7c5e22
Francisca Barros <kikas4323@gmail.com>
2021-05-07 12:46:57 +0100

    changed default error logger and added swagger documentation support

--- TQS_HW1-AirQualityApp/src/main/java/deti/tqs/fbarros/airqualityapp/controller/CityErrorController.java
+++ TQS_HW1-AirQualityApp/src/main/java/deti/tqs/fbarros/airqualityapp/controller/CityErrorController.java
@@ -2,18 +2,24 @@ package deti.tqs.fbarros.airqualityapp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CityErrorController {
+public class CityErrorController implements ErrorController {
 
     Logger logger = LoggerFactory.getLogger(CityController.class);
 
     @RequestMapping("/error404")
     public String throwerror404(){
-        logger.error("[ERROR 404] Invalid city name; Page unavailable.");
+        logger.error("[CityErrorController] Page unavailable");
         return "error404";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
     }
 
 }
